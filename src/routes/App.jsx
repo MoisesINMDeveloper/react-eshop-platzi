312d2de1b8ce50ca232569106632ae62007ca499
Moises Israel <MoisesINMDeveloper@hotmail.com>
2023-02-02 14:28:14 -0400

    Prueba para deploy

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
 	const initialState = useInitialState();
 	return (
 		<AppContext.Provider value={initialState}>
-			<BrowserRouter>
+			<BrowserRouter basename='/reactPractico'>
 				<Routes>
 					<Route path="/" element={<Layout />}>
 						<Route index element={<Home />} />
