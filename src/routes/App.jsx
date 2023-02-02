984fa1fc89aa9306fc3541c7edd479d3b4852969
Moises Israel <MoisesINMDeveloper@hotmail.com>
2023-02-02 17:55:09 -0400

    Edit App.jsx for deploy

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
 	const initialState = useInitialState();
 	return (
 		<AppContext.Provider value={initialState}>
-			<BrowserRouter>
+			<BrowserRouter basename={document.baseURI.substring(document.baseURI.indexOf(window.location.origin) + window.location.origin.length, document.baseURI.lastIndexOf('/'))}>
 				<Routes>
 					<Route path="/" element={<Layout />}>
 						<Route index element={<Home />} />
