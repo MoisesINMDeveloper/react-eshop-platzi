5455c965fbf0040bf440ae9b005e6d2380fc08ae
Moises Israel <MoisesINMDeveloper@hotmail.com>
2023-02-06 11:05:37 -0400

    Prueba deploy

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -20,18 +20,20 @@ const App = () => {
 	return (
 		<AppContext.Provider value={initialState}>
 			<BrowserRouter>
-				<Routes>
-					<Route exact path='/' element={<Home />} />
-					<Route exact path='/login' element={<Login />} />
-					<Route exact path='/recovery-password' element={<PasswordRecovery />} />
-					<Route exact path='/send-email' element={<SendEmail />} />
-					<Route exact path='/new-password' element={<NewPassword />} />
-					<Route exact path='/account' element={<MyAccount />} />
-					<Route exact path='/signup' element={<CreateAccount />} />
-					<Route exact path='/checkout' element={<Checkout />} />
-					<Route exact path='/orders' element={<Orders />} />
-					<Route path='*' element={<NotFound />} />
-				</Routes>
+				<Layout>
+					<Routes>
+						<Route exact path='/' element={<Home />} />
+						<Route exact path='/login' element={<Login />} />
+						<Route exact path='/recovery-password' element={<PasswordRecovery />} />
+						<Route exact path='/send-email' element={<SendEmail />} />
+						<Route exact path='/new-password' element={<NewPassword />} />
+						<Route exact path='/account' element={<MyAccount />} />
+						<Route exact path='/signup' element={<CreateAccount />} />
+						<Route exact path='/checkout' element={<Checkout />} />
+						<Route exact path='/orders' element={<Orders />} />
+						<Route path='*' element={<NotFound />} />
+					</Routes>
+				</Layout>
 			</BrowserRouter>
 		</AppContext.Provider>
 	);
