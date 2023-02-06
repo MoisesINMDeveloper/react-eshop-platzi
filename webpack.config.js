a5a40c0bfed2dd5d3836a1709a2d607e15ca0f63
Moises Israel <MoisesINMDeveloper@hotmail.com>
2023-02-06 11:19:05 -0400

    Prueba deploy

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/index.js',
   context: path.resolve(__dirname),
   output: {
-    path: path.resolve(__dirname, './build'),
+    path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js',
     publicPath: './',
   },
