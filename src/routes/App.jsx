f276bb3d464f27e3527ed1f7e35ff4284d9d8465
Moises Israel <MoisesINMDeveloper@hotmail.com>
2023-02-07 09:18:19 -0400

    Update

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -18,19 +18,19 @@ const App = () => {
   const initialState = useInitialState();
   return (
     <AppContext.Provider value={initialState} >
-       <BrowserRouter > {/* basename='/react-shop' */}
+      <BrowserRouter > {basename = '/react-shop'}
         <Layout>
           <Routes>
-            <Route exact path='/' element={<Home/>} />
-            <Route exact path='/login' element={<Login/>} />
-            <Route exact path='/recovery-password' element={<PasswordRecovery/>} />
-            <Route exact path='/send-email' element={<SendEmail/>} />
-            <Route exact path='/new-password' element={<NewPassword/>} />
-            <Route exact path='/account' element={<MyAccount/>} />
-            <Route exact path='/signup' element={<CreateAccount/>} />
-            <Route exact path='/checkout' element={<Checkout/>} />
-            <Route exact path='/orders' element={<Orders/>} /> 
-            <Route path='*' element={<NotFound/>} />
+            <Route exact path='*' element={<Home />} />
+            <Route exact path='/login' element={<Login />} />
+            <Route exact path='/recovery-password' element={<PasswordRecovery />} />
+            <Route exact path='/send-email' element={<SendEmail />} />
+            <Route exact path='/new-password' element={<NewPassword />} />
+            <Route exact path='/account' element={<MyAccount />} />
+            <Route exact path='/signup' element={<CreateAccount />} />
+            <Route exact path='/checkout' element={<Checkout />} />
+            <Route exact path='/orders' element={<Orders />} />
+            {/* <Route path='*' element={<NotFound />} /> */}
           </Routes>
         </Layout>
       </BrowserRouter>
